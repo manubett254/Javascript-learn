752f18ea8e8eab7b93c8e3525cc86a75eae988df
manubett254 <bettdevs@gmail.com>
2026-03-31 20:03:13 +0300

    lesson 1 and 2

---
+++ Week1/Lesson1/Lesson1.js
@@ -0,0 +1,4 @@
+// This is my first JavaScript code. I am  learning how to write JavaScript code and run it in the browser. I will start with a simple "Hello, World!" program.
+console.log("Hello, World!");
+console.log("Welcome to JavaScript programming.");  
+alert("I changed sth so it refreshes the page and runs the code again");
