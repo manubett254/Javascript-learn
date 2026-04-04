5d46a566e25d8e371a5e3f21bf3e960c75dcded9
manubett254 <bettdevs@gmail.com>
2026-04-04 10:34:26 +0300

    started Lesson 2

---
+++ Week1/Lesson2/script.js
@@ -0,0 +1,14 @@
+let name = "Manubett";
+
+console.log("hello " + name);   
+
+// alert("hello " + name)
+// cannot be a reserved keyword
+// should be meaningful 
+// cannot start with a number ( 1name)
+
+
+let firstName = "Emmanuel ";
+let lastName = "Bett";
+
+console.log(firstName+" "+ lastName);
